d2bfdef9301e2d50fc620d1d4ca6cb4ac25dd32f
Fernandez <Martinandresfernandez96@gmail.com>
2021-09-08 23:12:06 -0300

    Resumiendo el codigo en funciones

--- adobe.py
+++ adobe.py
@@ -1,43 +1,15 @@
 # %% LIBRERIAS
-import pymiere
 from pymiere import wrappers
+from pymiere.wrappers import get_system_sequence_presets
+from pymiere.wrappers import time_from_seconds
+from pytube import YouTube
+import pymiere
 import time
 import os
 import psutil
 import subprocess
 
 
-
-# %% EJECUCION
-
-# %% ABRIR ADOBE
-process = "Adobe Premiere Pro.exe"
-if not process in (i.name() for i in psutil.process_iter()):
-    print("Adobe execution in progress")
-    subprocess.Popen([r'F:/Adobe Premiere/Adobe Premiere Pro 2020/Adobe Premiere Pro.exe'])
-    print("Adobe execution complete")
-else:
-    print("Adobe is alredy open")
-
-# %% TEST
-print(pymiere.objects.app.isDocumentOpen())
-
-
-
-# %% CREATE PROJECT
-project_path = r"F:\Codigo\On github\media-python-premiere\adobe-test\test.prproj"
-# create new empty project
-pymiere.objects.app.newProject(project_path)  # from Premiere 2020
-
-
-# %%
-# open existing project
-pymiere.objects.app.openDocument(project_path)
-
-
-
-
-# %%
 # %% FUNCIONES
 def addVideo(clip,seq,inP,ouP):
     clip.setInPoint(inP, 4)
@@ -56,10 +28,126 @@ def cutVideo(clip, seq, inP, ouP, cut):
 
 
 
+def setup():
+    process = "Adobe Premiere Pro.exe"
+    if not process in (i.name() for i in psutil.process_iter()):
+        print("Adobe execution in progress")
+        subprocess.Popen([r"C:\Program Files\Adobe\Adobe Premiere Pro 2020\Adobe Premiere Pro.exe"])
+        time.sleep(25)
+        print("Adobe execution complete")
+    else:
+        print("Adobe is alredy open")
 
 
 
-cutVideo(clip, project.activeSequence, 10, 20, 15)
+    # CREAR PROYECTO
+    project_path = r'F:\Codigo\On github\media-python-premiere\adobe-test'
+    pymiere.objects.app.newProject(project_path)  # Create project
+
+
+
+    # CREAR SECUENCIA
+    # CONFIG
+    sequence_preset_path = get_system_sequence_presets(category="HDV", resolution=None, preset_name="HDV 1080p25")  
+    sequence_name = "My new sequence"
+
+    # CREATE
+    pymiere.objects.qe.project.newSequence(sequence_name, sequence_preset_path)
+
+    # OPEN
+    sequence = [s for s in pymiere.objects.app.project.sequences if s.name == sequence_name][0]
+    pymiere.objects.app.project.openSequence(sequenceID=sequence.sequenceID)
+
+
+
+
+# %%
+setup()
+
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %% SETUP
+# ABRIR ADOBE
+process = "Adobe Premiere Pro.exe"
+if not process in (i.name() for i in psutil.process_iter()):
+    print("Adobe execution in progress")
+    subprocess.Popen([r"C:\Program Files\Adobe\Adobe Premiere Pro 2020\Adobe Premiere Pro.exe"])
+    time.sleep(25)
+    print("Adobe execution complete")
+else:
+    print("Adobe is alredy open")
+
+
+
+# CREAR PROYECTO
+project_path = r'F:\Codigo\On github\media-python-premiere\adobe-test'
+pymiere.objects.app.newProject(project_path)  # Create project
+
+
+
+# %%
+# CREAR SECUENCIA
+# CONFIG
+sequence_preset_path = get_system_sequence_presets(category="HDV", resolution=None, preset_name="HDV 1080p25")  
+sequence_name = "My new sequence"
+
+# CREATE
+pymiere.objects.qe.project.newSequence(sequence_name, sequence_preset_path)
+
+# OPEN
+sequence = [s for s in pymiere.objects.app.project.sequences if s.name == sequence_name][0]
+pymiere.objects.app.project.openSequence(sequenceID=sequence.sequenceID)
+
+
+# %% AGREGAR VIDEO
+project = pymiere.objects.app.project
+media_path = r'F:\Codigo\On github\media-python-premiere\adobe-test\input\video.mp4'
+
+# import media into Premiere
+success = project.importFiles(
+    [media_path], # can import a list of media
+    suppressUI=True,
+    targetBin=project.getInsertionBin(),
+    importAsNumberedStills=False
+)
+# find media we imported
+items = project.rootItem.findItemsMatchingMediaPath(media_path, ignoreSubclips=False)
+# add clip to active sequence
+#project.activeSequence.videoTracks[0].insertClip(items[0], time_from_seconds(0))
+
+
+
 
 
 # %% Startup
@@ -69,7 +157,6 @@ if not project_opened:
     raise ValueError('please open a project')
 project = pymiere.objects.app.project
 
-
 # Open sequences in UI if none are active
 if not sequence_active:
     sequences = wrappers.list_sequences()
@@ -77,35 +164,24 @@ if not sequence_active:
         project.openSequence(sequenceID=seq.sequenceID)
     project.activeSequence = sequences[0]
 
-
-# list all videos clip in the active sequence
 clips = wrappers.list_video(project.activeSequence)
-clips[0].components
-
-# get sequence fps (timebase in ticks to be converted to frame per seconds)
-fps = 1/(float(project.activeSequence.timebase)/wrappers.TICKS_PER_SECONDS)
-print(f"sequence as a framerate of {fps} fps")
-
-# select the first clip in the timeline
-clips[0].setSelected(True, True)
-
-# %%
 
 
 
-
-
-
-
-# %% START ADOBE
-os.system(r'start F:\Adobe Premiere\Adobe Premiere Pro 2020\Adobe Premiere Pro.exe') 
-
+# %% CORTAR Y AGREGAR
+clip = items[0]
+cutVideo(clip, project.activeSequence, 100, 200, 150)
 
 
 
 
 # %%
 
-# %%
 
-# %%
+
+
+
+# %% DESCARGAR VIDEO
+yt = YouTube(input('Youtube video URL'))
+stream = yt.streams.filter(res=input('Resolution as 1080p/720p')).first()
+stream.download(input('Location to save video'))
