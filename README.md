e37b40242370357d133638ab91006687d7350fc2
Fernandez <Martinandresfernandez96@gmail.com>
2021-09-16 20:37:56 -0300

    I have to fix add video

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # media-python-premiere
+
+
+Freelance project to automatize an Adobe Premire Pro job.
+
+## instructions
+This bot can:
+- Download Youtube's videos.
+- Open Adobe.
+- Open/Create Project.
+- Create Sequence.
+- Import media folder.
+- Add video to sequence with cuts.
+- Add effects
+
+
+## To be added
+- UI
+- Encoder
--- adobe.py
+++ adobe.py
@@ -8,180 +8,222 @@ import time
 import os
 import psutil
 import subprocess
+import urllib.request
+
+
+# %%
+'''
+Configuracion previa:
+- Premiere version:
+- Premiere tiene que tener activada opcion: > Edit > Preferences
+                                            > Media > Default Media Scaling
+                                            > Scale to frame size
+- Tener creado proyecto con secuencias.
+
+
+STEPS:
+1 - Download video
+2 - Open Adobe and Template proyect
+3 - Import video folder
+4 - Cut video and select background
+5 - Background: None (black), White, Blur
+
+
+
+TODO:
+
+- PONER LINK DE DESCARGA Y QUE PIDA DIRECTAMENTE LOS TIEMPOS DE CORTE (SIEMPRE 480p)
+- AGREGAR EFECTO CROP: CROP TOP, BOTTOM, LEFT, RIGHT
+- NO SOLO BACKGROUND.PNG SINO QUE CUALQUIER OTRA IMAGEN
+- ESCALA VIDEO PRINCIPAL 185 - NO EN 16:9 - REVISAR
+- Adobe Media Enconder: PRESET > QUEUE > START QUEUE
+- EXPORTAR CADA VIDEO DE LA SECUENCIA COMO VIDEO1-VIDEO2-VIDEO3 (con nombres v1/v2/v3/etc... cada uno)
+- UI
+'''
+
 
 
 # %% FUNCIONES
-def addVideo(clip,seq,inP,ouP):
-    clip.setInPoint(inP, 4)
-    clip.setOutPoint(ouP, 4)
-    seq.videoTracks[0].insertClip(clip,0)
-
-def cutVideo(clip, seq, inP, ouP, cut):
-    clip.setInPoint(inP, 4)
-    clip.setOutPoint(cut, 4)
-    seq.videoTracks[0].insertClip(clip,0)
-
-    clip.setInPoint(cut, 4)
-    clip.setOutPoint(ouP, 4)
-    seq.videoTracks[0].insertClip(clip, cut-inP)
+def descargarvideo(youtubeurl, savepath):
+    yt = YouTube(youtubeurl)
+    stream = yt.streams.filter(res='480p').first()
+    stream.download(savepath)
+    try:
+        whitebackgroundurl = "https://i.imgur.com/AizUHEf.png"
+        urllib.request.urlretrieve(whitebackgroundurl, f"{savepath}/white-background.png")
+    except:
+        pass
 
 
-
-
-def setup():
+def setup(adobe_exe):
+    # ABRIR ADOBE
     process = "Adobe Premiere Pro.exe"
     if not process in (i.name() for i in psutil.process_iter()):
         print("Adobe execution in progress")
-        subprocess.Popen([r"C:\Program Files\Adobe\Adobe Premiere Pro 2020\Adobe Premiere Pro.exe"])
-        time.sleep(25)
+        subprocess.Popen([adobe_exe])
         print("Adobe execution complete")
     else:
         print("Adobe is alredy open")
 
 
+def abrirproyecto(template_file):
+    attempts = 0
+    while attempts < 10:
+        time.sleep(1)
+        try:
+            pymiere.objects.app.openDocument(template_file)
+            break
+        except:
+            attempts +=1
 
-    # CREAR PROYECTO
-    project_path = r'F:\Codigo\On github\media-python-premiere\adobe-test'
-    pymiere.objects.app.newProject(project_path)  # Create project
+
+def importvideo(media_path):
+    project = pymiere.objects.app.project
+    media_path = media_path
+
+    # import media into Premiere
+    success = project.importFiles(
+        [media_path], # can import a list of media
+        suppressUI=True,
+        targetBin=project.getInsertionBin(),
+        importAsNumberedStills=False
+    )
+    # find media we imported
+    items = project.rootItem.findItemsMatchingMediaPath(media_path, ignoreSubclips=False)
+    return project, items
+
+
+def listarvideos(items):
+    for i in range(len(items)):
+        print(f"Nombre del video {i}:", items[i].name)
+
+
+def addVideo(clip,seq,inP,ouP, track):
+    clip.setInPoint(inP, 4)
+    clip.setOutPoint(ouP, 4)
+    seq.videoTracks[track].insertClip(clip,0)
+
+
+def agregarvideo(background, clip, whiteclip, seq, inP, ouP):
+    addVideo(clip, seq, inP, ouP, track=1) # PRINCIPAL
+    if background == 0:
+        return
+    elif background == 1:
+        addVideo(clip, seq, inP, ouP, track=0) # BLUR
+    elif background == 2:
+        addVideo(whiteclip, seq, inP, ouP, track=0) # WHITE
+
+
+def agregarblur(clip):
+    qe_project = pymiere.objects.qe.project
+    track = qe_project.getActiveSequence().getVideoTrackAt(0)
+
+    for x in range(track.numItems):
+        clip = track.getItemAt(x)
+        clip.addVideoEffect(qe_project.getVideoEffectByName("Gaussian Blur"))
+
+    time.sleep(1)
+
+    for x in range(0, track.numItems-1):
+        clip = pymiere.objects.app.project.activeSequence.videoTracks[0].clips[x]
+
+        for component in clip.components:
+            if component.displayName == "Gaussian Blur":
+                break
+        else:
+            raise ValueError("No effect 'Gaussian Blur' found on first clip")
+
+        for property in component.properties:
+            if property.displayName == "Blurriness":
+                property.setValue(15, True)
+
+
+        for component in clip.components:
+            if component.displayName == "Motion":
+                break
+        else:
+            raise ValueError("No effect 'Motion' found on first clip")
+
+        for property in component.properties:
+            if property.displayName == "Scale":
+                property.setValue(500, True)
 
 
 
-    # CREAR SECUENCIA
-    # CONFIG
-    sequence_preset_path = get_system_sequence_presets(category="HDV", resolution=None, preset_name="HDV 1080p25")  
-    sequence_name = "My new sequence"
+def corregirescala(clip):
+    qe_project = pymiere.objects.qe.project
+    track = qe_project.getActiveSequence().getVideoTrackAt(1)
 
-    # CREATE
-    pymiere.objects.qe.project.newSequence(sequence_name, sequence_preset_path)
+    for x in range(0, track.numItems-1):
+        clip = pymiere.objects.app.project.activeSequence.videoTracks[1].clips[x]
 
-    # OPEN
+        for component in clip.components:
+            if component.displayName == "Motion":
+                break
+        else:
+            raise ValueError("No effect 'Motion' found on first clip")
+
+        for property in component.properties:
+            if property.displayName == "Scale":
+                property.setValue(185, True)
+
+
+# %% INPUT
+savepath = r'C:\Users\Martin\Downloads\Nueva carpeta'
+youtubeurl = r'https://youtu.be/5uQXXkpn6Mk'
+adobe_exe= r'C:\Program Files\Adobe\Adobe Premiere Pro 2020\Adobe Premiere Pro.exe'
+template_file = r"F:\Codigo\On github\media-python-premiere\adobe-test\Template.prproj"
+escala_principal = ""
+
+
+
+# %%
+descargarvideo(youtubeurl, savepath)
+setup(adobe_exe)
+abrirproyecto(template_file)
+project, items = importvideo(savepath)
+listarvideos(items)
+
+
+# %% ROTAR ENTRE SEQUENCES
+sequence_names= ["9:16", "4:5", "1:1", "16:9"]
+
+
+# %%
+clip = items[int(input("Numero: primero va el que queda ultimo, en ultimo lugar va el inicio del video"))]
+background = int(input("0 = Black, 1 = Blur, 2 = White"))
+whiteclip = [s for s in items if s.name == 'white-background.png'][0]
+inpoint = input("Inpoint en segundos")
+outpoint = input("Outpoint en segundos")
+
+for sequence in sequence_names:
+    sequence_name = sequence
     sequence = [s for s in pymiere.objects.app.project.sequences if s.name == sequence_name][0]
     pymiere.objects.app.project.openSequence(sequenceID=sequence.sequenceID)
+    agregarvideo(background,clip,whiteclip,project.activeSequence, inP=inpoint, ouP=outpoint)
+    agregarblur(clip)
 
 
 
 
 # %%
-setup()
+qe_project = pymiere.objects.qe.project
+track = qe_project.getActiveSequence().getVideoTrackAt(1)
 
-# %%
+for x in range(0, track.numItems-1):
+    clip = pymiere.objects.app.project.activeSequence.videoTracks[1].clips[x]
 
+    for component in clip.components:
+        if component.displayName == "Motion":
+            break
+    else:
+        raise ValueError("No effect 'Motion' found on first clip")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# %% SETUP
-# ABRIR ADOBE
-process = "Adobe Premiere Pro.exe"
-if not process in (i.name() for i in psutil.process_iter()):
-    print("Adobe execution in progress")
-    subprocess.Popen([r"C:\Program Files\Adobe\Adobe Premiere Pro 2020\Adobe Premiere Pro.exe"])
-    time.sleep(25)
-    print("Adobe execution complete")
-else:
-    print("Adobe is alredy open")
-
-
-
-# CREAR PROYECTO
-project_path = r'F:\Codigo\On github\media-python-premiere\adobe-test'
-pymiere.objects.app.newProject(project_path)  # Create project
-
+    for property in component.properties:
+        if property.displayName == "Scale":
+            property.setValue(185, True)
 
 
 # %%
-# CREAR SECUENCIA
-# CONFIG
-sequence_preset_path = get_system_sequence_presets(category="HDV", resolution=None, preset_name="HDV 1080p25")  
-sequence_name = "My new sequence"
-
-# CREATE
-pymiere.objects.qe.project.newSequence(sequence_name, sequence_preset_path)
-
-# OPEN
-sequence = [s for s in pymiere.objects.app.project.sequences if s.name == sequence_name][0]
-pymiere.objects.app.project.openSequence(sequenceID=sequence.sequenceID)
-
-
-# %% AGREGAR VIDEO
-project = pymiere.objects.app.project
-media_path = r'F:\Codigo\On github\media-python-premiere\adobe-test\input\video.mp4'
-
-# import media into Premiere
-success = project.importFiles(
-    [media_path], # can import a list of media
-    suppressUI=True,
-    targetBin=project.getInsertionBin(),
-    importAsNumberedStills=False
-)
-# find media we imported
-items = project.rootItem.findItemsMatchingMediaPath(media_path, ignoreSubclips=False)
-# add clip to active sequence
-#project.activeSequence.videoTracks[0].insertClip(items[0], time_from_seconds(0))
-
-
-
-
-
-# %% Startup
-# Check that a project is opened
-project_opened, sequence_active = wrappers.check_active_sequence(crash=False)
-if not project_opened:
-    raise ValueError('please open a project')
-project = pymiere.objects.app.project
-
-# Open sequences in UI if none are active
-if not sequence_active:
-    sequences = wrappers.list_sequences()
-    for seq in sequences:
-        project.openSequence(sequenceID=seq.sequenceID)
-    project.activeSequence = sequences[0]
-
-clips = wrappers.list_video(project.activeSequence)
-
-
-
-# %% CORTAR Y AGREGAR
-clip = items[0]
-cutVideo(clip, project.activeSequence, 100, 200, 150)
-
-
-
-
+addVideo(items[0], project.activeSequence, 0, 100, 2)
 # %%
-
-
-
-
-
-# %% DESCARGAR VIDEO
-yt = YouTube(input('Youtube video URL'))
-stream = yt.streams.filter(res=input('Resolution as 1080p/720p')).first()
-stream.download(input('Location to save video'))
